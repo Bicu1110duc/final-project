050e9a488a06dcd6ecb53cfff243fc3ee056269b
ngocduc <Bicu1110duc@gmail.com>
2025-05-19 22:51:34 +0700

    Fix Mail

--- docs/scripts/newsHandler.js
+++ docs/scripts/newsHandler.js
@@ -13,6 +13,12 @@ function renderNews() {
             link: "https://vnexpress.net/gia-vang-the-gioi-giam-tro-lai-4887088.html",
             image: "https://i1-kinhdoanh.vnecdn.net/2025/05/17/gold-2025-05-17T080119-167-174-2153-9366-1747443911.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=rJbr7MpTB9awI8fCYmvu5A",
             description: "Thỏa thuận thương mại Mỹ - Trung Quốc vẫn gây sức ép lớn lên giá vàng, bất chấp thông tin Mỹ bị hạ xếp hạng tín nhiệm."
+        },
+        {
+            title: "Giá vàng hôm nay 20/5/2025",
+            link: "https://baoquocte.vn/gia-vang-hom-nay-2052025-gia-vang-lai-nguoc-dong-canh-bao-khan-tu-thi-truong-trong-nuoc-than-trong-voi-ap-luc-co-the-den-bat-cu-luc-nao-314839.html",
+            image: "https://photo-baomoi.bmcdn.me/w250_r3x2/2025_05_19_194_52277869/cf7ea7490807e159b816.jpg.avif",
+            description: "Giá vàng 'lại ngược dòng', cảnh báo khẩn từ thị trường trong nước; Thận trọng với áp lực có thể đến bất cứ lúc nào"
         }
     ];
 
